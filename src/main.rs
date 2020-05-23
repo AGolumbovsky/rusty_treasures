cff005baaadd2c8875ac6f660b51a7d373544be9
Andriy Golumbovsky <agolumbovsky@gmail.com>
2020-05-23 00:37:04 -0400

    HashMap or_insert() getting a count

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    use std::collections::HashMap;
+
+    let text = "hello hello you pretty bastard";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        // count is set to 0 the first time, or_insert returns a mut ref
+        let count = map.entry(word).or_insert(0);
+
+        //count needs to be de-referenced so it can hold the previous value and be mutilated 
+        // I don't know, that's how I understand it now. Probably wrong
+        *count +=1; // count becomes 1 if it was nothing
+    }
+
+    // will print acutal count for each word in text
+    println!("{:?}", map); 
 }
